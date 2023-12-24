0bdf4b7de52e3abff1a6fede54596b1dea670ad0
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-12-24 12:57:14 -0500

    clean up static pages and add gif

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # ESL Worksheet Generator
-
+![Gif of ESL Worksheet Generator](working.gif)
 This is a Flask app that uses text scraped from [ESL Fast](https://www.eslfast.com/) to generate simple grammar worksheets. 
 
 The GitHub pages offer a glimpse into some of the possible worksheets it can generate.
 
 To use this, clone this repository. Install nltk and flask. Then run `main.py`.
+
 `git clone https://github.com/AkeBoss-tech/ESL-Worksheet`
+
 `pip install nltk`
+
 `pip install flask`
+
 `python main.py`
+
--- get_static_pages.py
+++ get_static_pages.py
@@ -1,17 +1,17 @@
 import requests
 local_host = 'http://127.0.0.1:5000/'
-# urls = ["generate/fill_worksheet", "generate/worksheet", "generate/rearrange_worksheet", "generate/vocab_worksheet"]
-urls = ["generate/worksheet", "generate/rearrange_worksheet", "generate/vocab_worksheet"]
+urls = ["generate/fill_worksheet", "generate/worksheet", "generate/rearrange_worksheet", "generate/vocab_worksheet"]
 
-""" 
 with open('index.html', 'w') as file:
     file.write(requests.get(local_host).content.decode('utf-8'))
 
 for url in urls:
-    with open(url.replace('/', '-') + '.html', 'w') as file:
+    with open("static/" + url.replace('/', '-') + '.html', 'w') as file:
         file.write(requests.get(local_host + url).content.decode('utf-8'))
- """
+
+urls = ["generate/worksheet", "generate/rearrange_worksheet", "generate/vocab_worksheet"]
+
 for level in ['1', '2', '3', '4', '5', '6']:
     for url in urls:
-        with open(url.replace('/', '-') + '-' + level + '.html', 'w') as file:
+        with open("static/"  + url.replace('/', '-') + '-' + level + '.html', 'w') as file:
             file.write(requests.get(local_host + url + '/' + level).content.decode('utf-8'))
